336c090645217cf22fb4d935cf9710b75224a6d8
mrgaren3 <mrchessarab2233@gmail.com>
2025-09-08 10:33:42 +0300

    lesson1,2

---
+++ lesson1.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+window = Tk()  # Create a window
+window.geometry("420x420")  # Set window size
+window.title("My First GUI Program")  # Set window title
+
+window.config(background="#5cfcff")  # Change background color
+
+window.mainloop()  # Keep window open
